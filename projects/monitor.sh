4fb6c43c593097d45c76cf5c26a0e544c5505568
Dillon Mendoza <mudd-fedora@localhost.localdomain>
2026-05-19 05:20:08 -0600

    Cleaned up ping output chatter so only the expected echo outputs were pushed through to n8n

--- projects/monitor.sh
+++ projects/monitor.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
 
 while IFS= read -r line; do
     device=$(echo $line | cut -d ',' -f 1)
     ip=$(echo $line | cut -d ',' -f 2)
-    ping $ip -c 3
+    ping $ip -c 3 > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "CONFIRMED | $device | $ip"
     else
         echo "UNCONFIRMED | $device | $ip"
     fi
-done < /usr/local/bin/client.conf
+done < /scripts/client.conf
